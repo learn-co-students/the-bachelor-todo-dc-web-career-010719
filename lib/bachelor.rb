d8b07d2360745f379c7994b97a227e02269c28fe
dcjones1 <dcejjones@gmail.com>
2019-01-06 22:22:24 -0500

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,63 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  full_name = ""
+  data[season].each do |people|
+   people.each do |k,v|
+    if v == "Winner"
+        full_name = people["name"]
+      end
+    end
+  end
+  full_name.split(" ").first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  x = ""
+  data.each do |season, array|
+    array.each do |people|
+      people.each do |k,v|
+        if v == occupation
+          x = people["name"]
+        end
+      end
+    end
+  end
+  x
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, contestants|
+    contestants.each do |person|
+      person.each do |attr, value|
+        if value == hometown
+          count += 1
+        end
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |person|
+      person.each do |attr, value|
+        if value == hometown
+          return person["occupation"]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+  data[season].each do |contest_hash|
+    ages << contest_hash["age"]
+  end
+  sum = 0
+  ages.each do |x|
+    sum += x.to_f
+  end
+  (sum / ages.size).round
 end
